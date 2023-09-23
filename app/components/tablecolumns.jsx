5f30cf02de0d3e24f5dc976a64047609afb4387d
Florien Niyongabo <waka.florien45@gmail.com>
2023-09-23 09:35:40 +0200

    message update

---
+++ app/components/tablecolumns.jsx
@@ -0,0 +1,53 @@
+export const TABLE_HEAD = [
+    "To",
+    "Subject",
+    "content",
+    "Status",
+    "Date time",
+    "Group",
+    ""
+];
+
+export const TABLE_ROWS = [
+    {
+        to: "Alexa Liras",
+        subject: "John Michael",
+        content: "Manager",
+        status: "23/04/18",
+        date: "23/04/18",
+        group: "Alexa Liras",
+    },
+    {
+        to: "Alexa Liras",
+        subject: "John Michael",
+        content: "Manager",
+        status: "23/04/18",
+        date: "23/04/18",
+        group: "Alexa Liras",
+    },
+    {
+        to: "Alexa Liras",
+        subject: "John Michael",
+        content: "Manager",
+        status: "23/04/18",
+        date: "23/04/18",
+        group: "Alexa Liras",
+    },
+    {
+        to: "Alexa Liras",
+        subject: "John Michael",
+        content: "Manager",
+        status: "23/04/18",
+        date: "23/04/18",
+        group: "Alexa Liras",
+    },
+    {
+        to: "Alexa Liras",
+        subject: "John Michael",
+        content: "Manager",
+        status: "23/04/18",
+        date: "23/04/18",
+        group: "Alexa Liras",
+    },
+
+];
--- app/dashboard/messages/page.js
+++ app/dashboard/messages/page.js
@@ -1,196 +1,149 @@
+"use client";
 import {
     Card, Typography, IconButton,
     SpeedDial,
     SpeedDialHandler,
-    SpeedDialContent,
-    SpeedDialAction,
 } from "../../../utils/material_tailwind";
 import {
-    PlusIcon,
-    HomeIcon,
-    CogIcon,
-    Square3Stack3DIcon,
     ChatBubbleBottomCenterTextIcon
 } from "@heroicons/react/24/outline";
+import { DefaultPagination } from "@/app/components/pagination";
+import { ConfirmModal } from "@/app/components/confirmModal";
+import { useState } from "react";
+import { TABLE_HEAD, TABLE_ROWS } from "@/app/components/tablecolumns";
 
 
-export default function Messages() {
-    const TABLE_HEAD = [
-        "To",
-        "Subject",
-        "content",
-        "Status",
-        "Date time",
-        "Group",
-        ""
-    ];
+export default function Messages(props) {
+    const [open, setOpen] = useState(false);
+    const handleOpen = () => setOpen(!open);
 
-    const TABLE_ROWS = [
-        {
-            to: "Alexa Liras",
-            subject: "John Michael",
-            content: "Manager",
-            status: "23/04/18",
-            date: "23/04/18",
-            group: "Alexa Liras",
-        },
-        {
-            to: "Alexa Liras",
-            subject: "John Michael",
-            content: "Manager",
-            status: "23/04/18",
-            date: "23/04/18",
-            group: "Alexa Liras",
-        },
-        {
-            to: "Alexa Liras",
-            subject: "John Michael",
-            content: "Manager",
-            status: "23/04/18",
-            date: "23/04/18",
-            group: "Alexa Liras",
-        },
-        {
-            to: "Alexa Liras",
-            subject: "John Michael",
-            content: "Manager",
-            status: "23/04/18",
-            date: "23/04/18",
-            group: "Alexa Liras",
-        },
-        {
-            to: "Alexa Liras",
-            subject: "John Michael",
-            content: "Manager",
-            status: "23/04/18",
-            date: "23/04/18",
-            group: "Alexa Liras",
-        },
-
-    ];
     return (
-        <div className="flex flex-col  p-4 space-y-11">
-            <header className="self-center">Messages</header>
-            <Card className="h-fit w-full overflow-scroll rounded-none">
-                <table className="w-full min-w-max table-auto text-left">
-                    <thead>
-                        <tr>
-                            {TABLE_HEAD.map((head) => (
-                                <th
-                                    key={head}
-                                    className="border-b border-blue-gray-100 bg-blue-gray-50 p-4"
-                                >
-                                    <Typography
-                                        variant="small"
-                                        color="blue-gray"
-                                        className="font-normal leading-none opacity-70"
+        <>
+            <ConfirmModal open={open} handleOpen={handleOpen} />
+            <div className="flex flex-col  p-4 space-y-11">
+                <header className="self-center">Messages</header>
+                <Card className="h-fit w-full rounded-none">
+                    <table className="w- min-w-max table-auto text-left">
+                        <thead>
+                            <tr>
+                                {TABLE_HEAD.map((head) => (
+                                    <th
+                                        key={head}
+                                        className="border-b border-blue-gray-100 bg-blue-gray-50 p-4"
                                     >
-                                        {head}
-                                    </Typography>
-                                </th>
-                            ))}
-                        </tr>
-                    </thead>
-                    <tbody>
-                        {TABLE_ROWS.map(({ to,
-                            subject,
-                            content,
-                            status,
-                            date,
-                            group }, index) => {
-                            const isLast = index === TABLE_ROWS.length - 1;
-                            const classes = isLast ? "p-4" : "p-4 border-b border-blue-gray-50";
+                                        <Typography
+                                            variant="small"
+                                            color="blue-gray"
+                                            className="font-normal leading-none opacity-70"
+                                        >
+                                            {head}
+                                        </Typography>
+                                    </th>
+                                ))}
+                            </tr>
+                        </thead>
+                        <tbody>
+                            {TABLE_ROWS.map(({ to,
+                                subject,
+                                content,
+                                status,
+                                date,
+                                group }, index) => {
+                                const isLast = index === TABLE_ROWS.length - 1;
+                                const classes = isLast ? "p-4" : "p-4 border-b border-blue-gray-50";
 
-                            return (
-                                <tr key={to}>
-                                    <td className={classes}>
-                                        <Typography
-                                            variant="small"
-                                            color="blue-gray"
-                                            className="font-normal"
-                                        >
-                                            {to}
-                                        </Typography>
-                                    </td>
-                                    <td className={classes}>
-                                        <Typography
-                                            variant="small"
-                                            color="blue-gray"
-                                            className="font-normal"
-                                        >
-                                            {subject}
-                                        </Typography>
-                                    </td>
-                                    <td className={classes}>
-                                        <Typography
-                                            variant="small"
-                                            color="blue-gray"
-                                            className="font-normal"
-                                        >
-                                            {content}
-                                        </Typography>
-                                    </td>
-                                    <td className={classes}>
-                                        <Typography
-                                            variant="small"
-                                            color="blue-gray"
-                                            className="font-normal"
-                                        >
-                                            {status}
-                                        </Typography>
-                                    </td>
-                                    <td className={classes}>
-                                        <Typography
-                                            variant="small"
-                                            color="blue-gray"
-                                            className="font-normal"
-                                        >
-                                            {date}
-                                        </Typography>
-                                    </td>
-                                    <td className={classes}>
-                                        <Typography
-                                            variant="small"
-                                            color="blue-gray"
-                                            className="font-normal"
-                                        >
-                                            {group}
-                                        </Typography>
-                                    </td>
+                                return (
+                                    <tr key={to}>
+                                        <td className={classes}>
+                                            <Typography
+                                                variant="small"
+                                                color="blue-gray"
+                                                className="font-normal"
+                                            >
+                                                {to}
+                                            </Typography>
+                                        </td>
+                                        <td className={classes}>
+                                            <Typography
+                                                variant="small"
+                                                color="blue-gray"
+                                                className="font-normal"
+                                            >
+                                                {subject}
+                                            </Typography>
+                                        </td>
+                                        <td className={classes}>
+                                            <Typography
+                                                variant="small"
+                                                color="blue-gray"
+                                                className="font-normal"
+                                            >
+                                                {content}
+                                            </Typography>
+                                        </td>
+                                        <td className={classes}>
+                                            <Typography
+                                                variant="small"
+                                                color="blue-gray"
+                                                className="font-normal"
+                                            >
+                                                {status}
+                                            </Typography>
+                                        </td>
+                                        <td className={classes}>
+                                            <Typography
+                                                variant="small"
+                                                color="blue-gray"
+                                                className="font-normal"
+                                            >
+                                                {date}
+                                            </Typography>
+                                        </td>
+                                        <td className={classes}>
+                                            <Typography
+                                                variant="small"
+                                                color="blue-gray"
+                                                className="font-normal"
+                                            >
+                                                {group}
+                                            </Typography>
+                                        </td>
 
-                                    <td className={classes}>
-                                        <Typography
-                                            as="a"
-                                            href="#"
-                                            variant="small"
-                                            color="blue-gray"
-                                            className="font-medium"
-                                        >
-                                            Edit
-                                        </Typography>
-                                    </td>
-                                </tr>
-                            );
-                        })}
-                    </tbody>
-                </table>
-            </Card>
-            <div className="flex items-center justify-end">
-                {/* <div className="absolute bottom-0 right-0"> */}
-                <SpeedDial>
-                    <SpeedDialHandler>
-                        <IconButton size="lg" className="rounded-full">
-                            <ChatBubbleBottomCenterTextIcon className="h-5 w-5 transition-transform group-hover:rotate-45" />
-                        </IconButton>
-                    </SpeedDialHandler>
-                    {/* <SpeedDialContent>
+                                        <td className={classes}>
+                                            <Typography
+                                                as="a"
+                                                href="#"
+                                                variant="small"
+                                                color="blue-gray"
+                                                className="font-medium"
+                                            >
+                                                Edit
+                                            </Typography>
+                                        </td>
+                                    </tr>
+                                );
+                            })}
+                        </tbody>
+                    </table>
+                </Card>
+                <DefaultPagination className="mt-4" />
+                <div className="flex items-center justify-end">
+                    {/* <div className="absolute bottom-0 right-0"> */}
+                    <SpeedDial>
+                        <SpeedDialHandler onClick={handleOpen}>
+                            <IconButton size="lg" className="rounded-full">
+                                <ChatBubbleBottomCenterTextIcon className="h-5 w-5 transition-transform group-hover:rotate-45" />
+                            </IconButton>
+                        </SpeedDialHandler>
+                        {/* <SpeedDialContent>
                         <SpeedDialAction>
                             <HomeIcon className="h-5 w-5" />
                         </SpeedDialAction>
                     </SpeedDialContent> */}
-                </SpeedDial>
-                {/* </div> */}
+                    </SpeedDial>
+                    {/* </div> */}
+                </div>
             </div>
-        </div>
+        </>
     )
 }
