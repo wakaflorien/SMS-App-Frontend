7c0e0a854aa2ebab2f7b65621496783c85a1fe33
Sezerano Jean Chrysostome <sezeranochrisostom123@gmail.com>
2023-10-23 09:43:34 +0200

    refactor: remove example endpoint

--- app/api/messages/route.ts
+++ app/api/messages/route.ts
@@ -1,11 +1,9 @@
 import { NextResponse } from "next/server";
 
 export async function GET(request: Request) {
-  const schoolId = request.headers.get("schoolId");
   const response = await fetch(
     `${process.env.HOST}/messages`
   );
   const result = await response.json();
-  console.log(result);
   return NextResponse.json({ ...result }, { status: response.status });
 }
