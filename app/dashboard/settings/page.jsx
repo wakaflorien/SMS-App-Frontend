51cb403e52d644847c4ec451bd041599ea07df56
sostene12 <ngarukiyimanasostene@gmail.com>
2024-07-16 16:57:59 +0200

    creating setting page

--- app/dashboard/settings/page.jsx
+++ app/dashboard/settings/page.jsx
@@ -1,11 +1,19 @@
 "use client";
 import React from "react";
-import { Layout, theme, Typography,Button } from "antd";
+import { Layout, theme, Typography, Button, Card, Flex,Image } from "antd";
 
 const { Content } = Layout;
 const { Title } = Typography;
+const { Meta } = Card;
+
+const myLoader=()=>{
+  return `https://os.alipayobjects.com/rmsportal/QBnOOoLaAfKPirc.png`;
+}
+
 
 const SettingsPage = () => {
+  const [value, setValue] = React.useState("horizontal");
+
   const {
     token: { colorBgContainer, borderRadiusLG },
   } = theme.useToken();
@@ -26,8 +34,13 @@ const SettingsPage = () => {
         borderRadius: borderRadiusLG,
       }}
     >
-      <Title level={4}>Profile</Title>
-      <Button type="primary" style={{width:100}}>Edit</Button>
+      <Flex gap="middle" vertical={false}>
+        <Image src="https://os.alipayobjects.com/rmsportal/QBnOOoLaAfKPirc.png" alt="Profile image" width={200} height={200} style={{  borderRadius: "20%" }} />
+        <Flex vertical gap="16px">
+          <Title level={4}>Profile</Title>
+        <Button type="primary">Save</Button>
+        </Flex>
+      </Flex>
     </Content>
   );
 };
